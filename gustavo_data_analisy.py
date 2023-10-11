fa3409c1fc173b3f6196d15bec512a0520172b7f
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-10-11 18:42:22 +0000

    Defini dois funções de analis de dados measure_tendensy e measure_dispersion ambas estão sem doc test e sem tratamento de exceção apenas

--- gustavo_data_analisy.py
+++ gustavo_data_analisy.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def summary(dataframe, column):
-    '''Realiza as medidas de resumo de uma coluna especificada.
+def measure_tendency(dataframe, column):
+    '''Realiza as Medidas de Tendência Central de uma coluna especificada.
 
     Parameters
     ----------
@@ -16,18 +16,42 @@ def summary(dataframe, column):
         
     Returns
     -------
-    summary
-        Uma tupla contendo cada medida de resumo, da forma [média, mediana, moda, desvio padrão, variância]
+    tendency_metrics
+        Uma tupla contendo cada medidas de tendência central, da forma [média, mediana, moda]
     '''
     media = dataframe[column].mean()
     mediana = dataframe[column].median()
     moda = dataframe[column].mode()
+    tendency_metrics = tuple(media, mediana, moda)
+    
+    return tendency_metrics
+
+def measure_dispersion(dataframe, column):
+    '''Realiza as Medidas de Dispersão de uma coluna especificada.
+
+    Parameters
+    ----------
+    dataframe: dataframe
+        O dataframe a ser analisado
+    column: str
+        O nome da coluna a ser resumida
+
+    Raises
+    ------
+        
+    Returns
+    -------
+    dispersion_metrics
+        Uma tupla contendo cada medidas de dispersão, da forma [desvio padrâo, variância, máximo, mínimo, amplitude]
+    '''
     desvio_padrao = dataframe[column].std().round(3)
-    variancia = 
+    variancia = dataframe[column].var()
+    maximo = dataframe[column].max()
+    minimo = dataframe[column].min()
+    amplitude = dataframe[column].max() - dataframe[column].min()
+    dispersion_metrics = tuple(desvio_padrao, variancia, maximo, minimo, amplitude)
     
-    summary = tuple(media, mediana, moda, desvio_padrao, variancia)
-    
-    return summary 
+    return dispersion_metrics
 
 def frequencia():
     pass
