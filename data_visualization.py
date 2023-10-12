3e5eef906c273752e69e4b24c7b4aa9e9190620c
Luís Felipe Marciano <lfamarciano@gmail.com>
2023-10-12 15:19:39 -0300

    visualização finalizada

--- data_visualization.py
+++ data_visualization.py
@@ -12,7 +12,9 @@ def visualizacao_gustavo(dataframe):
     ------
     TypeError
         O parametro dataframe não é um dataframe
-        
+    AttributeError
+        O parametro dataframe não é um dataframe
+
     Returns
     -------
     
@@ -31,9 +33,46 @@ def visualizacao_gustavo(dataframe):
         plt.xlabel('Estados') #define a label no eixo x
         plt.ylabel('Quantidade média de TVs') #define a label no eixo y
         plt.savefig('visualizacao_gustavo.png') #salva o plot em um arquivo
-    except TypeError:
+    except (TypeError,AttributeError):
         print(f'O argumeto passado não é do tipo pd.DataFrame') #avisa sobre o erro no argumento da função, não há como tratar de outra forma
         
+
+def visualizacao_marciano(dataframe):
+    '''Plota o gráfico que mostra a quantidade de escolas sem internet por estado, de acordo com a base de dados
+
+    Parameters
+    ----------
+    dataframe: dataframe
+        O dataframe a ser plotado
+
+    Raises
+    ------
+    TypeError
+        O parametro dataframe não é um dataframe
+        
+    Returns
+    -------
+    
+    >>> visualizacao_marciano('Erro')
+    O argumeto passado não é do tipo pd.DataFrame
+    '''
+    try:
+        colunas_selecionadas = dataframe[dataframe["NO_REGIAO"] == "Norte"]
+        escolas_sem_internet = colunas_selecionadas[colunas_selecionadas["IN_INTERNET"] == 0]
+        
+        n_escolas_sem_internet = escolas_sem_internet.groupby('NO_UF')['IN_INTERNET'].count().sort_values()
+
+        n_escolas_sem_internet.plot(kind='bar', color='red') # plota o grafico a partir da base de dados
+        plt.xlabel('Estado') # define label do eixo x
+        plt.ylabel('Número de Escolas') # define label do eixo y
+        plt.title('Quantidade de escolas sem acesso à Internet por Estado') #define título
+        plt.xticks(rotation=45) # roda o nome dos estados em 45 graus
+        plt.savefig("visualizacao_marciano.png", bbox_inches='tight') # salva o plot em um arquivo
+    except (TypeError, AttributeError):
+        print(f'O argumeto passado não é do tipo pd.DataFrame') #avisa sobre o erro no argumento da função, não há como tratar de outra forma
+
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import data_visualization as vis
 ## Tratamento dos dados
 dataframe_without_tratament = dc.csv_to_dataframe('censo_escolar_2021.csv') #lê o dado
 
-dataframe_no_irrelevant_coluns = dc.remove_colunas_irrelevantes(dataframe_without_tratament, 3600) #retira colunas com mais de 3600 NaN
+dataframe_names_corrected = dc.corrige_nome_estados(dataframe_without_tratament) # corrige o nome dos estados na coluna NO_UF
+
+dataframe_no_irrelevant_coluns = dc.remove_colunas_irrelevantes(dataframe_names_corrected, 3600) #retira colunas com mais de 3600 NaN
 
 dataframe_no_NaN = dataframe_no_irrelevant_coluns.dropna(axis = 0) #retira linhas com NaN
 
 dataframe = dc.replace_88888_values(dataframe_no_NaN) #altera os valores 88888 pelo maior valor imediatamente anteriror a ele
 
+
 ## Visualizações
 
 vis.visualizacao_gustavo(dataframe) #cria e salva a visualização do gustavo em um arquivo
+
+vis.visualizacao_marciano(dataframe) #cria e salva a visualização do marciano em um arquivo
