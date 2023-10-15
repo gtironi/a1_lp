43e0ce77975469d1d29b1c55e5ae550861dd5df6
Joao Pedro Tambosi <joaodupe2@gmail.com>
2023-10-15 20:43:27 -0300

    add tambosi viz com try except em data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def visualizacao_gustavo(dataframe):
     '''Plota o gráfico que mostra a quantidade média de TVs por escola, por estado
@@ -74,6 +75,53 @@ def visualizacao_marciano(dataframe):
         print(f'O argumeto passado não é do tipo pd.DataFrame') #avisa sobre o erro no argumento da função, não há como tratar de outra forma
 
 
+def visualizacao_tambosi(df):
+    """
+    Plota um histograma com a distribuição de datas de inicio de aulas por estado.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame contendo a data.
+
+    Returns:
+        None
+    """
+    try:
+        # 1. AGRUPAR POR REGIÃO
+        datas_estado = df.groupby("NO_REGIAO")
+    except (TypeError, AttributeError):
+        print('O argumeto passado não é do tipo pd.DataFrame') #avisa sobre o erro no argumento da função, não há como tratar de outra forma
+    
+    # 2. REALIZAR A ANALISE PARA CADA REGIAO
+    for region, data in datas_estado :   
+
+        try:
+            data_formatado = pd.to_datetime(data["DT_ANO_LETIVO_INICIO"], format='%d%b%Y:%H:%M:%S') # Transformar data em datetime
+        
+        except ValueError: # Provavelmente foi esquecida a limpeza da base!
+            print("Você esqueceu a limpeza da base!")
+            df = df.dropna()
+
+        # 3. AGRUPAR POR SEMANA
+        data[f"Semana do ano - {region}"] = data_formatado.dt.strftime('%Y-%U')
+
+        # 4. CONTAR A FREQUENCIA DE CADA SEMANA
+        semanal = data[f"Semana do ano - {region}"].value_counts()
+
+        # 5. ORDENAR O SERIES PELO INDEX ASC
+        semanal_ord = semanal.sort_index()
+
+        # 6. PARA CADA REGIAO, PLOTAR UM HISTOGRAMA DE DATA POR FREQUENCIA
+        plt.figure(figsize=(10, 3))
+        semanal_ord.plot(kind='bar', color='yellow')
+        plt.xlabel('Semana do ano')
+        plt.ylabel('Ocorrências')
+        plt.title(f'Distribuição de Data de Inicio das Aulas - {region}')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(f"(Tambosi) Distribuicao Inicio Aulas - {region}")
+        
+        
+            
 
 if __name__ == "__main__":
     import doctest
